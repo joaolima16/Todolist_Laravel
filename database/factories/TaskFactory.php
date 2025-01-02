fb90ba9d235e66f3924e3bba1f03bb21cc062bd6
joaolima16 <joao.lima@zanthus.com.br>
2025-01-01 21:33:16 -0300

    First commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TaskFactory extends Factory
+{
+
+    protected $model = Task::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(50),
+            'priority' => $this->faker->randomElement(['critical', 'medium', 'low']),
+            'user_id' => $this->faker->numberBetween(1, 10)
+        ];
+    }
+}
